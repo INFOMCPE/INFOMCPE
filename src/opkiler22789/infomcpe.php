332c8b635b42b7a331e462d8cfa264f945689b05
Aleksey Lozovagin <Oleksih@gmail.com>
2016-12-18 12:47:19 +0200

    Update infomcpe.php

--- src/opkiler22789/infomcpe.php
+++ src/opkiler22789/infomcpe.php
@@ -27,11 +27,11 @@ class infomcpe extends PluginBase{
             	
       case "infomcpe":
 if(count($args) == 0){
-$sender->sendMessage("§9§l—————§eINFOMCPE.RU§9—————\n§6/infomcpe download - §fскачать плагин \n§6/infomcpe pluginlist §7список установленых плагинов\n§6/infomcpe update - §fкриво работуюший autoupdater \n§9§l—————§eINFOMCPE§9—————");
+$sender->sendMessage("§9§l—————§eINFOMCPE.RU§9—————\n§6/infomcpe download - §fскачать плагин \n§6/infomcpe pluginlist - §fсписок установленых плагинов\n§6/infomcpe update - §fкриво работуюший autoupdater \n§9§l—————§eINFOMCPE.RU§9—————");
 }
 error_reporting(0);
       switch($args [0]){
-     $noperm = "§4Не достаточно прав";
+     
       case "download":
       if($sender->hasPermission("infomcpe.download")){
                 $data = json_decode(file_get_contents("http://infomcpe.ru/api.php?action=getResources&category_id=2"), true);
@@ -45,6 +45,59 @@ error_reporting(0);
       if($file){
         $this->download($this->getServer()->getPluginPath() . "[INFOMCPE.RU]{$args[1]} v{$version}.phar", $file);
         }else{
+            $sender->sendMessage("Ошибка");	
+        	}
+        }else{
+		$sender->sendMessage("§4Не достаточно прав");
+		}
+     break;
+     case "pluginlist":
+     if($sender->hasPermission("infomcpe.pluginlist")){
+     foreach ($this->getServer()->getPluginManager()->getPlugins() as $plugin) {
+                             $sender->sendMessage("{$plugin->getName()} v{$plugin->getDescription()->getVersion()}");
+                        } 
+	      }else{
+		$sender->sendMessage("§4Не достаточно прав");
+		}
+     break;
+     case "update":
+     if($sender->hasPermission("infomcpe.update")){
+     $this->autoupdate($sender);
+     }else{
+     	$sender->sendMessage("§4Не достаточно прав");
+     	}
+     break;
+     
+	}
+}
+}
+public function autoupdate($player){
+	            $data = json_decode(file_get_contents("http://infomcpe.ru/api.php?action=getResources&category_id=2"), true);
+                $count = 0;
+                foreach($data["resources"] as $resources){
+	            foreach ($this->getServer()->getPluginManager()->getPlugins() as $plugin) {
+		if($resources["title"] == $plugin->getName() && $resources["version_string"] != $plugin->getDescription()->getVersion()){
+			$file = Utils::getURL("http://infomcpe.ru/resources/{$resources['id']}/download?version={$resources['version_id']}");
+			$this->download($this->getServer()->getPluginPath() . "[INFOMCPE.RU]{$resources["title"]} v{$resources["version_string"]}.phar", $file);
+			$count++;
+			
+			}
+		}
+		}
+		if($count =<10 && $count =< 3){
+		$player->sendMessage("Обновлено {$count} плагинов");
+		}else{
+			$player->sendMessage("Обновлено {$count} плагинa{");
+			}
+	}
+public function download($path, $file){
+	   file_put_contents($path, $file);
+        $loader = new \pocketmine\plugin\PharPluginLoader($this->getServer());
+       $pl = $loader->loadPlugin($path);
+       $loader->enablePlugin($pl);
+	}
+	
+}
             $sender->sendMessage("Ошибка");	
         	}
         }else{
